3c573504d0f1ecde21492be5e566f33e88b9e9b7
Weicheng Pei <weicheng.pei@icloud.com>
2022-08-02 14:19:03 +0800

    Resize linear-scalar figures.

---
+++ thesis/figures/linear_scalar/p_vary.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from matplotlib import pyplot as plt
+plt.rcParams['font.sans-serif'] = ['Songti SC']
+
+if __name__ == '__main__':
+
+  plt.figure(figsize=(6,3))
+  plt.xlabel(r'$x$')
+  plt.ylabel(r'$U$')
+
+  x = np.linspace(0.0, 4.0, 1001)
+  u = 10.0 * (x - 2.) / np.abs(x - 2.)
+  u[len(u)//2] = 0.0
+  plt.plot(x, u, label='解析解')
+
+  names = ['p=3_h=2^-3', 'p=2_h=2^-3', 'p=1_h=2^-3']
+  labels = [r'$p=3,\quad h\approx 2^{-3}$', r'$p=2,\quad h\approx 2^{-3}$', r'$p=1,\quad h\approx 2^{-3}$']
+
+  data = []
+  for i in range(len(names)):
+    data.append(np.loadtxt('../../../mdpi/figures/linear_scalar/'+names[i]+'.csv', delimiter=',', skiprows=1))
+    print(data[i].shape)
+    plt.plot(data[i][:,3], data[i][:,0], label=labels[i])
+  plt.grid()
+  plt.legend()
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('p_vary.pdf')
