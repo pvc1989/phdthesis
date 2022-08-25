ba62fe994c5a777f2db8f978a8d09ebe00a38d23
Weicheng Pei <weicheng.pei@icloud.com>
2022-08-25 19:08:50 +0800

    Conform with C81 tables.

---
+++ thesis/figures/airfoil/low_aoa.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from cProfile import label
+import numpy as np
+
+
+if __name__ == '__main__':
+  from matplotlib import pyplot as plt
+
+  x = np.linspace(-32, 32, 33)
+
+  plt.figure(figsize=(6,3))
+  table = np.loadtxt('lift.csv', delimiter=',')
+  for i in range(0, 11, 2):
+    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i))
+  plt.legend()
+  plt.xticks(np.arange(-32, 33, step=8))
+  plt.grid()
+  plt.xlabel(r'$\alpha$ (${}^\circ$)')
+  plt.ylabel(r'$C_L$')
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('lift.pdf')
+
+  plt.figure(figsize=(6,3))
+  table = np.loadtxt('drag.csv', delimiter=',')
+  for i in range(0, 11, 2):
+    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i))
+  plt.legend()
+  plt.xticks(np.arange(-32, 33, step=8))
+  plt.grid()
+  plt.xlabel(r'$\alpha$ (${}^\circ$)')
+  plt.ylabel(r'$C_D$')
+  plt.tight_layout()
+  # plt.show()
+  plt.savefig('drag.pdf')
