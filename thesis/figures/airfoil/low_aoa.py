39066585996d37965c8d463048f7565de038d58f
Weicheng Pei <weicheng.pei@icloud.com>
2022-09-04 13:47:52 +0800

    Add markers to distinguish lines.

--- thesis/figures/airfoil/low_aoa.py
+++ thesis/figures/airfoil/low_aoa.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from cProfile import label
 import numpy as np
 
 
@@ -8,11 +7,12 @@ if __name__ == '__main__':
   from matplotlib import pyplot as plt
 
   x = np.linspace(-32, 32, 33)
+  markers = range(0, 11)
 
   plt.figure(figsize=(6,3))
   table = np.loadtxt('lift.csv', delimiter=',')
   for i in range(0, 11, 2):
-    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i))
+    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i), marker=markers[i])
   plt.legend()
   plt.xticks(np.arange(-32, 33, step=8))
   plt.grid()
@@ -25,7 +25,7 @@ if __name__ == '__main__':
   plt.figure(figsize=(6,3))
   table = np.loadtxt('drag.csv', delimiter=',')
   for i in range(0, 11, 2):
-    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i))
+    plt.plot(x, table[i], label='M = {0:.1f}'.format(0.1*i), marker=markers[i])
   plt.legend()
   plt.xticks(np.arange(-32, 33, step=8))
   plt.grid()
