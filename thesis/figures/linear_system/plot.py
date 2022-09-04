1780070ad015393295bdd2739e8bc3d3e93b44db
Weicheng Pei <weicheng.pei@icloud.com>
2022-09-04 19:34:46 +0800

    Reset line colors and styles.

--- thesis/figures/linear_system/plot.py
+++ thesis/figures/linear_system/plot.py
@@ -22,17 +22,17 @@ if __name__ == '__main__':
 
   names = ['lazy_p=2', 'eigen_p=2', 'lazy_p=3', 'eigen_p=3']
   labels = [r'$p=2, \mathtt{LazyWeno}$', r'$p=2, \mathtt{EigenWeno}$', r'$p=3, \mathtt{LazyWeno}$', r'$p=3, \mathtt{EigenWeno}$']
-  styles = ['g-.', 'y--', 'k-.', 'b--']
+  linestyles = [':', '--', '-.', (0, (3, 1, 1, 1, 1, 1))]
 
   # figure 1
   plt.figure(figsize=(6,3))
   plt.xlabel(r'$x$')
   plt.ylabel(r"$U_1$")
-  plt.plot(x, u[0], 'r-', label='解析解')
+  plt.plot(x, u[0], label='解析解')
   data = []
   for i in range(len(names)):
     data.append(np.loadtxt('../../../mdpi/figures/linear_system/'+names[i]+'.csv', delimiter=',', skiprows=1))
-    plt.plot(data[i][:,4], data[i][:,0], styles[i], label=labels[i])
+    plt.plot(data[i][:,4], data[i][:,0], linestyle=linestyles[i], label=labels[i])
   plt.grid()
   plt.legend()
   plt.tight_layout()
@@ -43,11 +43,11 @@ if __name__ == '__main__':
   plt.figure(figsize=(6,3))
   plt.xlabel(r'$x$')
   plt.ylabel(r"$U_2$")
-  plt.plot(x, u[1], 'r-', label='解析解')
+  plt.plot(x, u[1], label='解析解')
   data = []
   for i in range(len(names)):
     data.append(np.loadtxt('../../../mdpi/figures/linear_system/'+names[i]+'.csv', delimiter=',', skiprows=1))
-    plt.plot(data[i][:,4], data[i][:,1], styles[i], label=labels[i])
+    plt.plot(data[i][:,4], data[i][:,1], linestyle=linestyles[i], label=labels[i])
   plt.grid()
   plt.legend()
   plt.tight_layout()
@@ -64,13 +64,13 @@ if __name__ == '__main__':
   axins.set_xlim(1.3, 2.2)
   axins.set_ylim(-.1, 0.5)
   data = []
-  plt.plot(x, x*0, 'r-', label='解析解')
-  axins.plot(x, x*0, 'r-')
+  plt.plot(x, x*0, label='解析解')
+  axins.plot(x, x*0)
   for i in range(len(names)):
     data.append(np.loadtxt('../../../mdpi/figures/linear_system/'+names[i]+'.csv', delimiter=',', skiprows=1))
     error = (np.abs(data[i][:,0]-u[0]) + np.abs(data[i][:,1]-u[1]))
-    plt.plot(data[i][:,4], error, styles[i], label=labels[i], markersize=2)
-    axins.plot(data[i][:,4], error, styles[i], label=labels[i], markersize=2)
+    plt.plot(data[i][:,4], error, linestyle=linestyles[i], label=labels[i], markersize=2)
+    axins.plot(data[i][:,4], error, linestyle=linestyles[i], label=labels[i], markersize=2)
   ax.indicate_inset_zoom(axins, edgecolor="black")
   plt.grid()
   plt.legend()
