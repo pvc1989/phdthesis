f7f2c4a23d29646a5d2872927bf4ca1e9418d9cc
Weicheng <weicheng.pei@icloud.com>
2024-08-22 14:55:18 +0800

    Initialize the pages of phdthesis.

---
+++ assets/js/mathjax-load.js
@@ -0,0 +1,28 @@
+window.MathJax = {
+  loader: {load: ['[tex]/physics']},
+  tex: {
+    packages: {'[+]': ['physics']},
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    macros: {
+      coloneqq: "\\mathrel{:=}",
+      eqqcolon: "\\mathrel{=:}",
+    },
+  },
+};
+(function () {
+  function stripCDATA(x) {
+    if (x.startsWith('% <![CDATA[') && x.endsWith('%]]>'))
+      return x.substring(11,x.length-4);
+    return x;
+  }
+  document.querySelectorAll("script[type='math/tex']").forEach(function(el) {
+    el.outerHTML = "\\(" + stripCDATA(el.textContent) + "\\)";
+  });
+  document.querySelectorAll("script[type='math/tex; mode=display']").forEach(function(el) {
+    el.outerHTML = "\\[" + stripCDATA(el.textContent) + "\\]";
+  });
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
---
+++ presentation.md
@@ -0,0 +1,14 @@
+---
+layout: page
+title: Presentation
+---
+
+# 1. 引言
+
+# 2. 方法
+
+# 3. 结果
+
+$$
+\int_0^1 \dd{x}
+$$
